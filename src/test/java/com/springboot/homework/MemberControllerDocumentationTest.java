643bca732d3c21b3bd3d03d721b8b9b6870d015e
unknown <sw8865@naver.com>
2025-01-24 17:55:14 +0900

    커밋완료

--- src/test/java/com/springboot/homework/MemberControllerDocumentationTest.java
+++ src/test/java/com/springboot/homework/MemberControllerDocumentationTest.java
@@ -1,16 +1,48 @@
 package com.springboot.homework;
 
 import com.springboot.member.controller.MemberController;
+import com.springboot.member.dto.MemberDto;
+import com.springboot.member.entity.Member;
 import com.springboot.member.mapper.MemberMapper;
 import com.springboot.member.service.MemberService;
 import com.google.gson.Gson;
+import com.springboot.stamp.Stamp;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+import static com.springboot.util.ApiDocumentUtils.getRequestPreProcessor;
+import static com.springboot.util.ApiDocumentUtils.getResponsePreProcessor;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.startsWith;
+import static org.mockito.Mockito.doNothing;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.patch;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(MemberController.class)
 @MockBean(JpaMetamodelMappingContext.class)
@@ -27,19 +59,223 @@ public class MemberControllerDocumentationTest {
 
     @Autowired
     private Gson gson;
+    @Test
+    public void postMemberTest() throws Exception{
+        //given
+        MemberDto.Post post = new MemberDto.Post("lucky@google.com","김러키",
+                "010-0000-0000");
+        String content = gson.toJson(post);
 
+        Member mockResultMember = new Member();
+        mockResultMember.setMemberId(1L);
+
+        given(mapper.memberPostToMember(Mockito.any(MemberDto.Post.class)))
+                .willReturn(new Member());
+        given(memberService.createMember(Mockito.any(Member.class)))
+                .willReturn(mockResultMember);
+
+        URI uri = UriComponentsBuilder.newInstance().path("/v11/members").build().toUri();
+
+        //when
+        ResultActions actions = mockMvc.perform(
+                post(uri)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content)
+        );
+        //then
+        actions.andExpect(status().isCreated())
+                .andExpect(header().string("Location", is(startsWith("/v11/members/"))))
+                .andDo(document( //문서화 시작
+                        "post-member", //member컨트롤러의 postMember 문서화
+                        getRequestPreProcessor(),
+                        getResponsePreProcessor(),
+                        requestFields(
+                                List.of(
+                                        fieldWithPath("email").type(JsonFieldType.STRING).description("이메일"),
+                                        fieldWithPath("name").type(JsonFieldType.STRING).description("이름"),
+                                        fieldWithPath("phone").type(JsonFieldType.STRING).description("휴대폰 번호")
+                                )
+                        ),
+                        responseHeaders(
+                                headerWithName(HttpHeaders.LOCATION).description("Location header, 등록된 리소스의 RUI")
+                        )
+                ));
+    }
+
+    @Test
+    public void patchMemberTest() throws Exception{
+        //given
+        long memberId = 1L;
+        MemberDto.Patch patch = new MemberDto.Patch(memberId,"김러키",
+                "010-0000-0000", Member.MemberStatus.MEMBER_ACTIVE);
+        String content = gson.toJson(patch);
+
+        MemberDto.Response response = new MemberDto.Response(
+                memberId,
+                "sw8865@cat.house",
+                "김러키",
+                "010-0000-0000",
+                Member.MemberStatus.MEMBER_ACTIVE,
+                new Stamp()
+        );
+
+        given(mapper.memberPatchToMember(Mockito.any(MemberDto.Patch.class))).willReturn(new Member());
+        given(memberService.updateMember(Mockito.any(Member.class))).willReturn(new Member());
+        given(mapper.memberToMemberResponse(Mockito.any(Member.class))).willReturn(response);
+
+        //uri를 api툴이 인식을 못한다.
+        //when
+        ResultActions actions = mockMvc.perform(
+                patch("/v11/members/{memberId}", memberId)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content)
+        );
+        //then
+        actions.andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.name").value(patch.getName()))
+                .andExpect(jsonPath("$.data.memberStatus").value(patch.getMemberStatus().getStatus()))
+                .andExpect(jsonPath("$.data.phone").value(patch.getPhone()))
+                .andDo(document( //문서화 시작
+                        "patch-member", //member컨트롤러의 patchMember 문서화
+                        getRequestPreProcessor(),
+                        getResponsePreProcessor(),
+                        pathParameters(
+                                parameterWithName("memberId").description("회원 식별자")
+                        ),
+                        requestFields(
+                                List.of(
+                                        fieldWithPath("memberId").type(JsonFieldType.NUMBER).description("회원 식별자").ignored(),
+                                        fieldWithPath("name").type(JsonFieldType.STRING).description("이름").optional(),
+                                        fieldWithPath("phone").type(JsonFieldType.STRING).description("휴대폰 번호").optional(),
+                                        fieldWithPath("memberStatus").type(JsonFieldType.STRING).description("회원 상태: 활동중 / 휴면 상태 / 탈퇴 상태\"").optional()
+                                )
+                        ),
+                        responseFields(
+                                List.of(
+                                        fieldWithPath("data").type(JsonFieldType.OBJECT).description("결과 데이터"),
+                                        fieldWithPath("data.memberId").type(JsonFieldType.NUMBER).description("회원 식별자"),
+                                        fieldWithPath("data.email").type(JsonFieldType.STRING).description("이메일"),
+                                        fieldWithPath("data.name").type(JsonFieldType.STRING).description("이름"),
+                                        fieldWithPath("data.phone").type(JsonFieldType.STRING).description("휴대폰 번호"),
+                                        fieldWithPath("data.memberStatus").type(JsonFieldType.STRING).description("회원 상태: 활동중 / 휴면 상태 / 탈퇴 상태"),
+                                        fieldWithPath("data.stamp").type(JsonFieldType.NUMBER).description("스탬프 갯수")
+                                )
+                        )
+                ));
+    }
     @Test
     public void getMemberTest() throws Exception {
         // TODO 여기에 MemberController의 getMember() 핸들러 메서드 API 스펙 정보를 포함하는 테스트 케이스를 작성 하세요.
+        //given
+        long memberId = 1L;
+        MemberDto.Response response = new MemberDto.Response(
+                memberId,
+                "lucky@cat.com",
+                "김러키",
+                "010-1234-1234",
+                Member.MemberStatus.MEMBER_ACTIVE,
+                new Stamp()
+        );
+
+        given(memberService.findMember(Mockito.anyLong())).willReturn(new Member());
+        given(mapper.memberToMemberResponse(Mockito.any(Member.class))).willReturn(response);
+        //when
+        ResultActions actions = mockMvc.perform(
+                get("/v11/members/{memberId}", memberId)
+                        .accept(MediaType.APPLICATION_JSON)
+        );
+
+        // then
+        actions.andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.memberId").value(response.getMemberId()))
+                .andExpect(jsonPath("$.data.email").value(response.getEmail()))
+                .andExpect(jsonPath("$.data.name").value(response.getName()))
+                .andExpect(jsonPath("$.data.phone").value(response.getPhone()))
+                .andDo(document(
+                        "get-member",
+                        getRequestPreProcessor(),
+                        getResponsePreProcessor(),
+                        pathParameters(
+                                parameterWithName("memberId").description("회원 식별자")
+                        ),
+                        responseFields(
+                                List.of(
+                                        fieldWithPath("data").type(JsonFieldType.OBJECT).description("결과 데이터"),
+                                        fieldWithPath("data.memberId").type(JsonFieldType.NUMBER).description("회원 식별자"),
+                                        fieldWithPath("data.email").type(JsonFieldType.STRING).description("이메일"),
+                                        fieldWithPath("data.name").type(JsonFieldType.STRING).description("이름"),
+                                        fieldWithPath("data.phone").type(JsonFieldType.STRING).description("휴대폰 번호"),
+                                        fieldWithPath("data.memberStatus").type(JsonFieldType.STRING).description("회원 상태: 활동중 / 휴면 상태 / 탈퇴 상태"),
+                                        fieldWithPath("data.stamp").type(JsonFieldType.NUMBER).description("스탬프 갯수")
+                                )
+                        )
+                ));
     }
 
     @Test
     public void getMembersTest() throws Exception {
         // TODO 여기에 MemberController의 getMembers() 핸들러 메서드 API 스펙 정보를 포함하는 테스트 케이스를 작성 하세요.
+        Page<Member> page = new PageImpl<>(List.of(new Member(), new Member()));
+
+        List<MemberDto.Response> responses = List.of(
+                new MemberDto.Response(1L, "lucky@google.com", "김럭키", "010-1234-1234", Member.MemberStatus.MEMBER_ACTIVE, new Stamp()),
+                new MemberDto.Response(2L, "song@naver.com", "송모근", "010-5678-1234", Member.MemberStatus.MEMBER_ACTIVE, new Stamp())
+        );
+        given(memberService.findMembers(Mockito.anyInt(),Mockito.anyInt())).willReturn(page);
+        given(mapper.membersToMemberResponses(Mockito.anyList())).willReturn(responses);
+
+        URI uri = UriComponentsBuilder.newInstance().path("/v11/members").build().toUri();
+
+        ResultActions actions = mockMvc.perform(
+                get(uri)
+                        .param("page", "1")
+                        .param("size", "10")
+                        .accept(MediaType.APPLICATION_JSON)
+        );
+
+        actions.andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.length()").value(responses.size()))
+                .andDo(document(
+                        "get-members",
+                        getRequestPreProcessor(),
+                        getResponsePreProcessor(),
+                        responseFields(
+                                List.of(
+                                        //회원 응답 정보
+                                        fieldWithPath("data").type(JsonFieldType.ARRAY).description("결과 데이터"),
+                                        fieldWithPath("data[].memberId").type(JsonFieldType.NUMBER).description("회원 식별자"),
+                                        fieldWithPath("data[].email").type(JsonFieldType.STRING).description("이메일"),
+                                        fieldWithPath("data[].name").type(JsonFieldType.STRING).description("이름"),
+                                        fieldWithPath("data[].phone").type(JsonFieldType.STRING).description("휴대폰 번호"),
+                                        fieldWithPath("data[].memberStatus").type(JsonFieldType.STRING).description("회원 상태: 활동중 / 휴면 상태 / 탈퇴 상태"),
+                                        fieldWithPath("data[].stamp").type(JsonFieldType.NUMBER).description("스탬프 갯수"),
+                                        //페이지 응답 정보
+                                        fieldWithPath("pageInfo").type(JsonFieldType.OBJECT).description("페이지 정보"),
+                                        fieldWithPath("pageInfo.page").type(JsonFieldType.NUMBER).description("현재 페이지 번호"),
+                                        fieldWithPath("pageInfo.size").type(JsonFieldType.NUMBER).description("페이지 당 항목 수"),
+                                        fieldWithPath("pageInfo.totalElements").type(JsonFieldType.NUMBER).description("전체 항목 수"),
+                                        fieldWithPath("pageInfo.totalPages").type(JsonFieldType.NUMBER).description("전체 페이지 수")
+                                )
+                        )
+                ));
     }
 
     @Test
     public void deleteMemberTest() throws Exception {
         // TODO 여기에 MemberController의 deleteMember() 핸들러 메서드 API 스펙 정보를 포함하는 테스트 케이스를 작성 하세요.
+        long memberId = 1L;
+        doNothing().when(memberService).deleteMember(Mockito.anyLong());
+        ResultActions actions = mockMvc.perform(delete("/v11/members/{memberId}",memberId));
+        actions.andExpect(status().isNoContent())
+                .andDo(document(
+                        "delete-member",
+                        getRequestPreProcessor(),
+                        getResponsePreProcessor(),
+                        pathParameters(
+                                parameterWithName("memberId").description("회원 식별자")
+                        ))
+                );
     }
 }
